b31d243cbf5672b2dc1fe66e6aa0a2bfdfafd603
Tomas Paez <paeztms@gmail.com>
2023-07-23 00:54:46 -0300

    make custom store to access firebase auth state

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { writable } from 'svelte/store';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyAZ_4i1lHDqe1rkvTm8fCUaokp_jq4syRY',
@@ -13,7 +14,31 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore();
-const auth = getAuth();
-const storage = getStorage();
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
+
+function userStore() {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser');
+		const { subscribe } = writable<User | null>(null);
+		return { subscribe };
+	}
+
+	const { subscribe } = writable(auth.currentUser ?? null, set => {
+		unsubscribe = onAuthStateChanged(auth, user => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return {
+		subscribe,
+	};
+}
+
+export const user = userStore();
