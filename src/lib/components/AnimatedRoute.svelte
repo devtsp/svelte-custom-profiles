371f8d7547bbba68ea6aba6459f8a5521098a688
Tomas Paez <paeztms@gmail.com>
2023-07-23 00:19:23 -0300

    add component to render animated transitions

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { page } from '$app/stores';
+</script>
+
+<!-- #key: re-render content when right side value changes (will re-trigger animation)  -->
+{#key $page.url}
+	<div in:fly={{ x: '-100%', duration: 500 }}>
+		<slot />
+	</div>
+{/key}
