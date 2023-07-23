f230fa1cc7bf2ccf0d5c9a8e04e6171ca452f369
Tomas Paez <paeztms@gmail.com>
2023-07-23 00:20:00 -0300

    add animated transtions as wrapper of the login layout body

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 </script>
+<AnimatedRoute>
 	<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
 		<div class="card-body items-center text-center">
 			<slot />
 		</div>
 	</main>
+</AnimatedRoute>
