b03eb6838a0ff68206beae6c5da382c94d2b9748
Tomas Paez <paeztms@gmail.com>
2023-07-23 21:54:14 -0300

    create a svelte store to read the logged user information from firestore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { doc, getFirestore, onSnapshot } from 'firebase/firestore';
 import { getAuth, onAuthStateChanged, type User } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
-import { writable } from 'svelte/store';
+import { derived, writable, type Readable } from 'svelte/store';
 
 const firebaseConfig = {
 	apiKey: 'AIzaSyAZ_4i1lHDqe1rkvTm8fCUaokp_jq4syRY',
@@ -41,4 +41,44 @@ function userStore() {
 	};
 }
 
-export const user = userStore();
+/**
+ * Reusable function to subscribe to different firestore collections
+ * @param path a path to the resource
+ * @returns a store with  realtime updates on document data
+ */
+function docStore<T>(path: string) {
+	let unsubscribe: () => void;
+	const docRef = doc(db, path);
+	const { subscribe } = writable<T | null>(null, set => {
+		unsubscribe = onSnapshot(docRef, snapshot => {
+			set((snapshot.data() as T) ?? null);
+		});
+		return () => unsubscribe();
+	});
+	return {
+		subscribe,
+		ref: docRef,
+		id: docRef.id,
+	};
+}
+
+const user = userStore();
+
+interface UserData {
+	username: string;
+	bio: string;
+	photoURL: string;
+	links: any[];
+}
+
+// Combine already created user store with derived svelte method
+const userData: Readable<UserData | null> = derived(user, ($user, set) => {
+	// If user's logged in return subscription to another store holding user info in the cloud
+	if ($user) {
+		return docStore<UserData>(`users/${$user.uid}`).subscribe(set);
+	} else {
+		set(null);
+	}
+});
+
+export { user, userData, docStore };
